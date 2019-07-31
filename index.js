587c1535d214c3ed3d43d90fd060841a2bf6a2dd
Joel Edwards <joeledwards@gmail.com>
2019-07-31 12:40:41 -0600

    feat: added handling for .publishConfig.registry in package.json

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 const axios = require('axios')
-const ChangesStream = require('changes-stream');
+const ChangesStream = require('changes-stream')
 const eos = require('end-of-stream')
 const exec = require('child_process').exec
 const fs = require('fs')
+const gunzip = require('gunzip-maybe')
+const tar = require('tar-stream')
 const mkdirp = require('mkdirp')
 const path = require('path')
-const uuid = require('uuid')
+const { ReadableStreamBuffer, WritableStreamBuffer } = require('stream-buffers')
+const uuid = require('uuid/v4')
 const yargs = require('yargs')
+const zlib = require('zlib')
 
 const defaultSourceCouchdb = 'http://localhost:15984/registry'
 const defaultTargetRegistry = 'http://localhost:18080'
 const defaultLastSequence = 0
 const defaultHaltOnError = false
 const defaultTmpFolder = '/tmp/tarballs'
+const defaultRemovePublishRegistry = false
 
 const args = yargs
   .env('PNEUMATIC_TUBES')
@@ -36,6 +41,11 @@ const args = yargs
     desc: 'halt when an error occurs',
     default: defaultHaltOnError
   })
+  .option('--remove-publish-registry', {
+    type: 'boolean',
+    desc: 'if .publishConfig.registry exists in package.json, remove it rather than replacing it with the target-registry',
+    default: defaultRemovePublishRegistry
+  })
   .option('tmp-folder', {
     type: 'string',
     desc: 'scratch directory for package tarballs',
@@ -50,9 +60,11 @@ class Tubes {
     this.lastSequence = opts.lastSequence || defaultLastSequence
     this.haltOnError = opts.haltOnError || defaultHaltOnError
     this.tmpFolder = opts.tmpFolder || defaultTmpFolder
+    this.removePublishRegistry = opts.removePublishRegistry || defaultRemovePublishRegistry
 
     mkdirp.sync(this.tmpFolder)
   }
+
   series () {
     const changes = new ChangesStream({
       db: this.sourceCouchdb, // full database URL
@@ -73,6 +85,7 @@ class Tubes {
       }
     })
   }
+
   async processChange (change) {
     const versions = Object.keys(change.doc.versions)
     for (var i = 0, version; (version = change.doc.versions[versions[i]]) !== undefined; i++) {
@@ -92,27 +105,92 @@ class Tubes {
       }
     }
   }
+
   download (tarball) {
-    const filename = path.resolve(this.tmpFolder, `${uuid.v4()}.tgz`)
+    const filename = path.resolve(this.tmpFolder, `${uuid()}.tgz`)
     return axios({
       method: 'get',
       url: tarball,
       responseType: 'stream'
     })
-    .then(function(response) {
-      return new Promise((resolve, reject) => {
-        const stream = response.data.pipe(fs.createWriteStream(filename))
-        eos(stream, err => {
-          if (err) return reject(err)
-          else return resolve()
+      .then(function (response) {
+        return new Promise((resolve, reject) => {
+          const stream = response.data.pipe(fs.createWriteStream(filename))
+          eos(stream, err => {
+            if (err) return reject(err)
+            else return resolve()
+          })
         })
       })
-    })
-    .then(() => {
-      console.info(`finished writing ${filename}`)
-      return filename
+      .then(() => {
+        console.info(`finished writing ${filename}`)
+        return filename
+      })
+  }
+
+  correctPublishRegistry (tarball) {
+    // Correct the .publishConfig.registry property if it is defined
+
+    return new Promise((resolve, reject) => {
+      let correctedPublishRegistry = false
+      const gzip = () => zlib.createGzip()
+      const newTarball = path.resolve(this.tmpFolder, `${uuid()}.tgz`)
+      const srcStream = fs.createReadStream(tarball)
+      const dstStream = fs.createWriteStream(newTarball)
+
+      // Check whether the property is defined in the tarball
+      const done = error => {
+        if (error) {
+          reject(error)
+        } else {
+          pack.finalize()
+          resolve(correctedPublishRegistry ? newTarball : tarball)
+        }
+      }
+
+      const extract = tar.extract()
+      const pack = tar.pack()
+
+      extract.on('entry', (header, stream, callback) => {
+        if (header.name === 'package.json') {
+          const inBuffer = new WritableStreamBuffer()
+          const outBuffer = new ReadableStreamBuffer()
+
+          stream
+            .pipe(inBuffer)
+            .on('end', () => {
+              const pkg = JSON.parse(inBuffer.getContentsAsString('utf8'))
+              if (pkg.publishConfig.registry == null) {
+                outBuffer.put(inBuffer.getContents())
+              } else {
+                correctedPublishRegistry = true
+                if (this.removePublishRegistry) {
+                  delete pkg.publishConfig.registry
+                  if (Object.keys(pkg.publishConfig).length < 1) {
+                    delete pkg.publishConfig
+                  }
+                } else {
+                  pkg.publishConfig.registry = this.targetRegistry
+                }
+                outBuffer.put(Buffer.from(JSON.stringify(pkg)))
+              }
+              outBuffer.pipe(pack.entry(header, callback))
+            })
+        } else {
+          // Forward the entry into the new tarball unmodified.
+          stream.pipe(pack.entry(header, callback))
+        }
+      })
+
+      extract.on('finish', () => {
+        done()
+      })
+
+      srcStream.pipe(gunzip()).pipe(extract)
+      pack.pipe(gzip()).pipe(dstStream)
     })
   }
+
   publish (filename) {
     return new Promise((resolve, reject) => {
       exec(`npm --registry=${this.targetRegistry} publish ${filename}`, {
